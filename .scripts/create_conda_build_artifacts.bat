3e79a8bf47c7fa5fd5f031b8b2de688f02acaeba
Michał Górny <mgorny@gentoo.org>
2026-07-31 18:15:59 +0200

    MNT: Re-rendered with conda-smithy 2026.6.15.dev11+g5b2bd15e9.d20260630 and conda-forge-pinning 2026.07.31.05.55.43
    Other tools:
    - conda-build 26.5.0
    - rattler-build 0.71.0
    - rattler-build-conda-compat 1.4.17

--- .scripts/create_conda_build_artifacts.bat
+++ .scripts/create_conda_build_artifacts.bat
@@ -42,7 +42,7 @@ if not "%ARTIFACT_UNIQUE_ID%" == "%ARTIFACT_UNIQUE_ID:~0,80%" (
     set ARTIFACT_UNIQUE_ID=%CI_RUN_ID%_%CONFIG_SHORT%
 )
 
-set "ZSTD=--use-compress-prog=zstd -T0 -0 -vv"
+set "ZSTD=--zstd"
 
 rem Mirror the logic from create_conda_build_artifacts.sh.tmpl.
 rem Note that paths to tar must use forward slashes.
