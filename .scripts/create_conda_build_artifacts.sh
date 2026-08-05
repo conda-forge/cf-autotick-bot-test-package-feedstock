4f96eed5bf392c08b00ae9df00f0cde45e169672
Michał Górny <mgorny@gentoo.org>
2026-08-05 20:51:01 +0200

    MNT: Re-rendered with conda-smithy 0.0.0 and conda-forge-pinning 2026.08.05.10.37.37
    Other tools:
    - conda-build 26.5.0
    - rattler-build 0.71.0
    - rattler-build-conda-compat 1.4.17

--- .scripts/create_conda_build_artifacts.sh
+++ .scripts/create_conda_build_artifacts.sh
@@ -129,8 +129,10 @@ if [[ ! -z "$BLD_ARTIFACT_PREFIX" ]]; then
     fi
 
     if [[ -s ${BLD_ARTIFACT_PATH} ]]; then
+        echo
         echo "Build artifact archive:"
         ls -l -h "${BLD_ARTIFACT_PATH}"
+        echo
         echo "BLD_ARTIFACT_NAME: $BLD_ARTIFACT_NAME"
         echo "BLD_ARTIFACT_PATH: $BLD_ARTIFACT_PATH"
 
@@ -161,15 +163,21 @@ if [[ ! -z "$WRK_ARTIFACT_PREFIX" && -n ${WORK_PATHS[@]} ]]; then
         WRK_ARTIFACT_PATH=${WRK_ARTIFACT_PATH/%.tar.zst/-broken&}
     fi
 
-    echo "WRK_ARTIFACT_NAME: $WRK_ARTIFACT_NAME"
-    echo "WRK_ARTIFACT_PATH: $WRK_ARTIFACT_PATH"
+    if [[ -s ${WRK_ARTIFACT_PATH} ]]; then
+        echo
+        echo "Work directory artifact archive:"
+        ls -l -h "${WRK_ARTIFACT_PATH}"
+        echo
+        echo "WRK_ARTIFACT_NAME: $WRK_ARTIFACT_NAME"
+        echo "WRK_ARTIFACT_PATH: $WRK_ARTIFACT_PATH"
 
-    if [[ "$CI" == "azure" ]]; then
-        echo "##vso[task.setVariable variable=WRK_ARTIFACT_NAME]$WRK_ARTIFACT_NAME"
-        echo "##vso[task.setVariable variable=WRK_ARTIFACT_PATH]$WRK_ARTIFACT_PATH"
-    elif [[ "$CI" == "github_actions" ]]; then
-        echo "WRK_ARTIFACT_NAME=$WRK_ARTIFACT_NAME" >> $GITHUB_OUTPUT
-        echo "WRK_ARTIFACT_PATH=$WRK_ARTIFACT_PATH" >> $GITHUB_OUTPUT
+        if [[ "$CI" == "azure" ]]; then
+            echo "##vso[task.setVariable variable=WRK_ARTIFACT_NAME]$WRK_ARTIFACT_NAME"
+            echo "##vso[task.setVariable variable=WRK_ARTIFACT_PATH]$WRK_ARTIFACT_PATH"
+        elif [[ "$CI" == "github_actions" ]]; then
+            echo "WRK_ARTIFACT_NAME=$WRK_ARTIFACT_NAME" >> $GITHUB_OUTPUT
+            echo "WRK_ARTIFACT_PATH=$WRK_ARTIFACT_PATH" >> $GITHUB_OUTPUT
+        fi
     fi
 fi
 
@@ -187,15 +195,21 @@ if [[ ! -z "$ENV_ARTIFACT_PREFIX" && -n ${ENVIRONMENT_PATHS[@]} ]]; then
         ENV_ARTIFACT_PATH=${ENV_ARTIFACT_PATH/%.tar.zst/-broken&}
     fi
 
-    echo "ENV_ARTIFACT_NAME: $ENV_ARTIFACT_NAME"
-    echo "ENV_ARTIFACT_PATH: $ENV_ARTIFACT_PATH"
+    if [[ -s ${ENV_ARTIFACT_PATH} ]]; then
+        echo
+        echo "Build environment artifact archive:"
+        ls -l -h "${ENV_ARTIFACT_PATH}"
+        echo
+        echo "ENV_ARTIFACT_NAME: $ENV_ARTIFACT_NAME"
+        echo "ENV_ARTIFACT_PATH: $ENV_ARTIFACT_PATH"
 
-    if [[ "$CI" == "azure" ]]; then
-        echo "##vso[task.setVariable variable=ENV_ARTIFACT_NAME]$ENV_ARTIFACT_NAME"
-        echo "##vso[task.setVariable variable=ENV_ARTIFACT_PATH]$ENV_ARTIFACT_PATH"
-    elif [[ "$CI" == "github_actions" ]]; then
-        echo "ENV_ARTIFACT_NAME=$ENV_ARTIFACT_NAME" >> $GITHUB_OUTPUT
-        echo "ENV_ARTIFACT_PATH=$ENV_ARTIFACT_PATH" >> $GITHUB_OUTPUT
+        if [[ "$CI" == "azure" ]]; then
+            echo "##vso[task.setVariable variable=ENV_ARTIFACT_NAME]$ENV_ARTIFACT_NAME"
+            echo "##vso[task.setVariable variable=ENV_ARTIFACT_PATH]$ENV_ARTIFACT_PATH"
+        elif [[ "$CI" == "github_actions" ]]; then
+            echo "ENV_ARTIFACT_NAME=$ENV_ARTIFACT_NAME" >> $GITHUB_OUTPUT
+            echo "ENV_ARTIFACT_PATH=$ENV_ARTIFACT_PATH" >> $GITHUB_OUTPUT
+        fi
     fi
 fi
 
